2ade5ed7886b7a4980942cd06e76ce49fd66b5c8
avsetsin <mail@sjors.ru>
2022-04-03 11:09:58 +0300

    Initial commit

---
+++ src/common/logger/logger.module.ts
@@ -0,0 +1,20 @@
+import {
+  jsonTransport,
+  simpleTransport,
+  LoggerModule as Logger,
+} from '@lido-nestjs/logger';
+import { ConfigModule, ConfigService, LogFormat } from 'common/config';
+
+export const LoggerModule = Logger.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => {
+    const level = configService.get('LOG_LEVEL');
+    const format = configService.get('LOG_FORMAT');
+
+    const isJSON = format === LogFormat.json;
+    const transports = isJSON ? jsonTransport() : simpleTransport();
+
+    return { level, transports };
+  },
+});
